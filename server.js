ef0332d99f7b1a4e5f4ca9af8118c1f76dfd827a
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-30 21:50:51 -0700

    maybe this'll work

--- server.js
+++ server.js
@@ -7,5 +7,10 @@ main().catch(err => console.log(err));
 async function main() {
   // await mongoose.connect('mongodb://127.0.0.1:27017/test');
   await mongoose.connect('mongodb+srv://nicoledhsu:RLriZ7QL64oOTTwK@cluster0.wjwjdfs.mongodb.net/?retryWrites=true&w=majority');
+
+  app.listen(3000, () => {
+    console.log("listening")
+  })
+
 }
 
