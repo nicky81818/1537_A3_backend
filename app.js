ecb496ded87452723345d1238b7c1110801156cc
NICOLE_HSU <nicole.d.hsu@gmail.com>
2023-03-31 19:55:48 -0700

    implemented searching and sorting logic

--- app.js
+++ app.js
@@ -26,10 +26,17 @@ app.post('/search', async (req, res) => {
         console.log(selectionArgument)
         var projectionArgument = {
         }
-        if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == false) {
+        if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == true) {
+            projectionArgument = {"name": 1, "weight": 1, "_id": 0}
+            console.log(projectionArgument)
+        } else if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == false) {
             projectionArgument = {"name": 1, "_id": 0}
-        } else {
 
+        } else if (req.body.projectionFilters.name == false && req.body.projectionFilters.weight == true){
+            projectionArgument = {"weight": 1, "_id": 0}
+        }
+        else {
+            projectionArgument = {}
         }
         const result = await unicornModel.find(
             selectionArgument, projectionArgument
@@ -40,10 +47,81 @@ app.post('/search', async (req, res) => {
         res.json(result)
     }
     else if (req.body.type == 'weightSearch'){
+        var selectionArgument = {}
+        if (req.body.weight_lower && req.body.weight_upper) {
+            selectionArgument = {
+                weight: {$gte: req.body.weight_lower},
+                weight: {$lte: req.body.weight_upper}
+            }
+        }
+        else if (!req.body.weight_lower) {
+            selectionArgument = {
+                weight: {$lte: req.body.weight_upper}
+            }
+        }
+        else if (!req.body.weight_upper) {
+            selectionArgument = {
+                weight: {$lte: req.body.weight_upper}
+            }
+        }
+        if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == true) {
+            projectionArgument = {"name": 1, "weight": 1, "_id": 0}
+            console.log(projectionArgument)
+        } else if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == false) {
+            projectionArgument = {"name": 1, "_id": 0}
 
+        } else if (req.body.projectionFilters.name == false && req.body.projectionFilters.weight == true){
+            projectionArgument = {"weight": 1, "_id": 0}
+        }
+        else {
+            projectionArgument = {}
+        }
+        const result = await unicornModel.find(
+            selectionArgument, projectionArgument
+            // , projectionArgument
+            // name: req.body.name
+        )
+
+        res.json(result)
     }
     else if (req.body.type == 'foodSearch'){
+        var selectionArgument = {}
+        var loves_list = req.body.loves
+        console.log(loves_list.length)
+        if (loves_list.length == 2) {
+            selectionArgument =  {
+                $and: [
+                    {loves: req.body.loves[0]},
+                    {loves: req.body.loves[1]}
+                ]
+            }
+        }
+        else if (loves_list.length == 1) {
+            selectionArgument =  {
+                loves: loves_list[0]
+            }
+        }
+        console.log(selectionArgument)
+        var projectionArgument = {}
+        if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == true) {
+            projectionArgument = {"name": 1, "weight": 1, "_id": 0}
+            console.log(projectionArgument)
+        } else if (req.body.projectionFilters.name == true && req.body.projectionFilters.weight == false) {
+            projectionArgument = {"name": 1, "_id": 0}
 
+        } else if (req.body.projectionFilters.name == false && req.body.projectionFilters.weight == true){
+            projectionArgument = {"weight": 1, "_id": 0}
+        }
+        else {
+            projectionArgument = {}
+        }
+        const result = await unicornModel.find(
+            selectionArgument, projectionArgument
+            // , projectionArgument
+            // name: req.body.name
+        )
+
+        res.json(result)
     }
 })
 
